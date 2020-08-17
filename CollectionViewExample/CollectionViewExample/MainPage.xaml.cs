5af6dd398cd3fdd5195657d26775cdcaab6304c4
Kevin Anderson <kevin@andersonplanet.com>
2020-08-17 13:25:26 -0600

    Add project files.

---
+++ CollectionViewExample/CollectionViewExample/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CollectionViewExample
+{
+    public partial class MainPage : ContentPage
+    {
+        MainViewModel vm = new MainViewModel();
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = vm;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            vm.InitializeAsync();
+        }
+    }
+}
---
+++ CollectionViewExample/CollectionViewExample/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CollectionViewExample
+{
+    public class AdviceCard
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class MainViewModel
+    {
+        public ObservableCollection<AdviceCard> AdviceCards { get; set; }
+
+        public MainViewModel()
+        {
+            AdviceCards = new ObservableCollection<AdviceCard>();
+        }
+
+        public void InitializeAsync()
+        {
+            AdviceCards.Add(new AdviceCard { Content = "something 1", Title = "title 1" });
+            AdviceCards.Add(new AdviceCard { Content = "something 2", Title = "title 2" });
+        }
+    }
+
+    
+}
